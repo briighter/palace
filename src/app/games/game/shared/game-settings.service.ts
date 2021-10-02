4c1761fc8cde9f70826f56c4a9f6d4fa254c208f
Jason <jaylima0@gmail.com>
2021-10-02 18:29:47 -0400

    CLEAN

--- src/app/games/game/shared/game-settings.service.ts
+++ src/app/games/game/shared/game-settings.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameSettingsService {
+  // Observable string sources
+  gameNameSource = new Subject<string>();
+
+  // Observable string streams
+  missionAnnounced$ = this.gameNameSource.asObservable();
 
   constructor() { }
+
+  // Service message commands
+  // announceMission(mission: string) {
+  //   this.missionAnnouncedSource.next(mission);
+  // }
 }
