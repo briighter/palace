2dd449c8a8e6ad57a63fd81e02b8a6dcc10ac50f
Jason <limaeight@gmail.com>
2021-10-05 09:20:05 -0400

    clean

--- src/app/games/game/shared/game-settings/game-settings.component.ts
+++ src/app/games/game/shared/game-settings/game-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,6 +10,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class GameSettingsComponent implements OnInit {
   @Output() goBackEvent = new EventEmitter<boolean>();
   @Input() game;
+  finalTime: string;
+
+  length = new FormControl('');
+  timeMinutes = new FormControl('');
+  timeSeconds = new FormControl('');
+  fontSize = new FormControl('');
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
