abc4ebf65c0f7e3c2cb661065001a810a795badf
Jason <jaylima0@gmail.com>
2021-10-20 18:43:14 -0400

    add login comp

---
+++ src/app/shared/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Browser } from '@capacitor/browser';
+import { mergeMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+
+  constructor(public auth: AuthService) {}
+
+  login() {
+    this.auth
+      .buildAuthorizeUrl()
+      .pipe(mergeMap((url) => Browser.open({ url, windowName: '_self' })))
+      .subscribe();
+  }
+}
---
+++ src/app/shared/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { LoginComponent } from '../login/login.component';
+
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ]
+})
+export class SharedModule { }
