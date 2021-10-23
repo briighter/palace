f60079860a34fff2caef807644bc69b97ab943d0
Jason <limaeight@gmail.com>
2021-10-23 01:24:48 -0400

    fix mosule error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import  domain  from '../../auth_config.json';
-import  clientId  from '../../auth_config.json';
+import authInfo from '../../auth_config.json';
 
 export const environment = {
   production: false,
   auth: {
-    domain,
-    clientId,
+    domain: authInfo.domain,
+    clientId: authInfo.clientId,
     redirectUri: window.location.origin,
   },
 };
