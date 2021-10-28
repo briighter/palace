ed09c5d473d3e5b452477b9d7bc24c552655d915
Jason <limaeight@gmail.com>
2021-10-27 23:19:33 -0400

    clean

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -6,34 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-  themeToggle;
+  // Query for the toggle that is used to change between themes
+  toggle = document.querySelector('#themeToggle') as HTMLIonToggleElement;
+  prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-  constructor() { }
+  constructor() {
+    if (this.toggle) {
+      // Listen for the toggle check/uncheck to toggle the dark class on the <body>
+      this.toggle.addEventListener('ionChange', (ev) => {
+        document.body.classList.toggle('dark', (<any>ev).detail.checked);
+      });
+    }
+
+    // Listen for changes to the prefers-color-scheme media query
+    this.prefersDark.addListener((e) => this.checkToggle(e.matches));
+
+  }
 
   ngOnInit() {
+    if (this.toggle) {
+      this.checkToggle(this.prefersDark.matches);
+    }
   }
-
-  // Query for the toggle that is used to change between themes
-  const toggle = document.querySelector('#themeToggle');
-
-  // Listen for the toggle check/uncheck to toggle the dark class on the <body>
-  toggle.addEventListener('ionChange', (ev) => {
-    document.body.classList.toggle('dark', ev.detail.checked);
-  });
-
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-  // Listen for changes to the prefers-color-scheme media query
-  prefersDark.addListener((e) => checkToggle(e.matches));
 
   // Called when the app loads
-  function loadApp() {
-    checkToggle(prefersDark.matches);
-  }
+  // loadApp() {
+  //   this.checkToggle(this.prefersDark.matches);
+  // }
 
   // Called by the media query to check/uncheck the toggle
-  function checkToggle(shouldCheck) {
-    toggle.checked = shouldCheck;
+  checkToggle(shouldCheck) {
+    this.toggle.checked = shouldCheck;
   }
 
 }
