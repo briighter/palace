513f947e277d51db03bd0bc3cb4fec4f3ecbb3e0
Jason <jaylima0@gmail.com>
2021-11-06 23:23:17 -0400

    CLEAN

--- src/app/profile/profile-stats/profile-stats.component.ts
+++ src/app/profile/profile-stats/profile-stats.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+interface gameHistory {
+  game: string;
+  numberOfItems: string;
+  numberofSeconds: string;
+  result: string;
+}
+
 @Component({
   selector: 'app-profile-stats',
   templateUrl: './profile-stats.component.html',
   styleUrls: ['./profile-stats.component.scss'],
 })
 export class ProfileStatsComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {}
