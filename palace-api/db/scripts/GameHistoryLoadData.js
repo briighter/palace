a42adea5a8f8739dd2174e40c1743290dda4cb1c
Jason <limaeight@gmail.com>
2021-12-19 07:42:03 -0500

    clean gamedata

--- palace-api/db/scripts/GameHistoryLoadData.js
+++ palace-api/db/scripts/GameHistoryLoadData.js
@@ -11,7 +11,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 
 console.log("Importing game history into DynamoDB. Please wait.");
 
-var allGameHistory = JSON.parse(fs.readFileSync('../sample_data/gamedata.json', 'utf8'));
+var allGameHistory = JSON.parse(fs.readFileSync('../sample_data/gamedata2.json', 'utf8'));
 allGameHistory.forEach(function (data) {
 
     var params = {
